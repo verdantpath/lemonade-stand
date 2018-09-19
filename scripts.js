a3e573a894fcf13a3dedfd3e9469fdb1f6e4e89a
verdantpath <m_dittmann@yahoo.com>
2018-09-19 00:06:43 -0700

    update html, js

--- scripts.js
+++ scripts.js
@@ -6,10 +6,39 @@ let upgrades = 0;
 let revPerSecond = 0;
 
 const lemonadePrice = 5;
+const employeePrice = 50;
+
 const btnSellLemonade = document.getElementById('btn-sell-lemonade');
 btnSellLemonade.addEventListener('click', function() {
   let output = document.getElementById('lemonade-sold');
   lemonadeSold++;
   output.innerHTML = lemonadeSold;
+  calculateTotalMonies();
   console.log('clicking');
 });
+
+const btnHireEmployee = document.getElementById('btn-hire-employee');
+btnHireEmployee.addEventListener('click', function() {
+  let output = document.getElementById('employees-hired');
+  employeesHired++;
+  output.innerHTML = employeesHired;
+  calculateTotalMonies();
+});
+
+const calculateTotalMonies = () => {
+
+  if (employeesHired > 0) {
+    setTimeout(start, 0);
+
+    function start() {
+      setInterval(increase, 1000);
+    }
+
+    function increase() {
+        totalMonies++;
+        document.getElementById('total-monies').innerHTML = totalMonies;
+    }
+  }
+  totalMonies = (lemonadeSold * lemonadePrice) - (employeesHired * employeePrice);
+  document.getElementById('total-monies').innerHTML = totalMonies;
+};
